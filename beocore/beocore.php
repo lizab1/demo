33718f1a50717ab26e462cddc028490ff4409f42
root <root@lb.(none)>
2016-12-23 02:25:21 +0200

    Init

---
+++ beocore/beocore.php
@@ -0,0 +1,9 @@
+<?php
+namespace beoLab6;
+
+class beocore{
+public function __construct(){
+echo "<a>5+ !</a>";
+}
+}
+?>
---
+++ web/index.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+$test = new beoLab6\beocore(); 
+?>
